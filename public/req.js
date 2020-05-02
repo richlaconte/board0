e7156853a192b9a0c3440cbfbc5c2f25d402a71c
Richard LaConte <38797549+pakjones@users.noreply.github.com>
2020-05-01 20:48:34 -0400

    Addressed issue #9

--- public/req.js
+++ public/req.js
@@ -6,12 +6,15 @@ const joinRoom = (roomName, playerName) => {
 }
 
 const requestMove = (oldX, oldY, newX, newY) => {
-    socket.emit('move', {
-        oldX,
-        oldY,
-        newX,
-        newY
-    })
+    // Check to make sure the move request isn't to the current position
+    if (oldX !== newX || oldY !== newY) {
+        socket.emit('move', {
+            oldX,
+            oldY,
+            newX,
+            newY
+        })
+    }
 }
 
 const sendMessage = (text) => {
